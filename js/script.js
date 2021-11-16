650650f8465fac251c003cb895be7ac4c3a4e7e0
sara_h <smoke_screen16@hotmail.com>
2021-11-15 21:25:41 -0500

    Added keydown event listener

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+
+document.addEventListener("keydown", function (e) {
+    var key = e.key;
+    if (key === "1") {
+        box1.innerText = "🌈";
+    } else if (key === "2") {
+        box2.innerText = "♎";
+    } else if (key === "3") {
+        box3.innerText = "🍜"; 
+    };
+});
